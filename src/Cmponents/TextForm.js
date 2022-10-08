261a6424e0b30bb41ffd7b7231ed6adda9a4a67a
Anurag Biswal <anurag.biswal0702@gmail.com>
2022-10-08 21:55:00 +0530

    Replaced text.

--- src/Cmponents/TextForm.js
+++ src/Cmponents/TextForm.js
@@ -38,6 +38,13 @@ export default function TextForm(props)
       let newText = text.split(/[ ]+/);
       setText(newText.join(" "))
     }
+    const handleReplace = ()=>{
+      let repval=prompt("Enter the new text:")
+      let tobereplaced = new RegExp(repval,'g');
+      let toreplace=prompt("Enter the text that you want to replace with:")
+      let newText= text.replace(tobereplaced,toreplace);
+         setText(newText);
+    }
    
     const [text, setText] = useState('');
   return (
@@ -55,6 +62,7 @@ export default function TextForm(props)
 <button  disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
 <button  disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy</button>
 <button  disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+<button  disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleReplace}>Replce-Text</button>
 
 </div>
 <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
